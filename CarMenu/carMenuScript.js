c7bace7ee2639fe9ff323548f5641f11847ca11d
Admir Jareb <110131840+xJareb@users.noreply.github.com>
2023-12-02 11:37:50 +0100

    Generated UI

---
+++ CarMenu/carMenuScript.js
@@ -0,0 +1,172 @@
+//generate random numbers for user who is not registred
+let randomUserName = () =>{
+    let userName = document.querySelector('.user p');
+    let randomNumber =  Math.random() * 1000;
+    userName.innerText += parseInt(randomNumber);
+}
+//import data from API
+let body = document.querySelector('.car-list');
+
+let dataLoad = () =>{
+    fetch(`https://65665f6864fcff8d730ebcb1.mockapi.io/Cars`)
+            .then(
+                r => {
+                    if (r.status !== 200) {
+                        alert("Server javlja grešku: " + r.status);
+                        return;
+                    }
+
+                    r.json().then(obj => {
+                        for (const x of obj) {
+                            body.innerHTML +=`<div class="car">
+                            <div class="carImage">
+                                <img src="${x.image}" alt="">
+                            </div>
+                            <div class="carTitle">
+                                <p>${x.name}</p>
+                            </div>
+                            <div class="carPerInfo">
+                                <p>${x.price}KM/dan</p>
+                                <button onclick="showPaymentType(this); check(this)">Rezervisi</button>
+                            </div>
+                            <div class="carDetails" onclick="showDetails(this)">
+                                <p>Detalji o automobilu</p>
+                            </div>
+                            <div class="detailsInfo">
+                                <div class="informations">
+                                    <p>Klasa: ${x.class}</p>
+                                    <p>Gorivo: ${x.fuel}</p>
+                                    <p>Broj vrata: ${x.doors}</p>
+                                </div>
+                                <div class="informations">
+                                    <p>Motor: ${x.engine} cm3</p>
+                                    <p>Snaga: ${x.power} KW</p>
+                                    <p>Godina: ${x.year}</p>
+                                </div>
+                            </div>
+                        </div>`
+                        }
+                    });
+                }
+            )
+            .catch(
+                err => {
+                    alert("Error: " + err);
+                }
+            );
+}
+dataLoad();
+
+function check (element){
+    //implement later
+    element.setAttribute('disabled','true');
+}
+
+let realBody = document.querySelector('body');
+let paymentBody = document.querySelector('.car-container');
+
+let showPaymentType = (element) =>{
+
+
+    let paymentMethod = document.createElement('div');
+    paymentMethod.innerHTML = `<div class="payment-container">
+    <h1>Odaberite nacin placanja</h1>
+        <div class="radioInput">
+        <div class="cashPayment">
+          <input type="radio" id="cash" name="fav_language" value="cash">
+          <label for="cash">Gotovina</label><br>
+        </div>
+        <div class="creditCardPayment">
+          <input type="radio" id="creditCard" name="fav_language" value="creditCard">
+          <label for="creditCard">Kartica</label><br>
+        </div>
+        </div>
+        <div class="buttonComands">
+        <button onclick="backToPage()">Odustani</button>
+        <button onclick="goToNextPage()">Nastavi</button>
+        </div>
+    </div>`
+    paymentBody.appendChild(paymentMethod);
+    realBody.style.overflowY = 'hidden';
+    userContainer.removeEventListener('click',pageEdit);
+}
+
+let goToNextPage = () =>{
+    let checkedCreditCard = false;
+    let checkedCash = false;
+
+    let cashPayment = document.querySelector('.cashPayment input');
+    let creditCardPayment = document.querySelector('.creditCardPayment input');
+
+    if(cashPayment.checked === true){
+        checkedCash = true;
+    }
+    if(checkedCash === true){
+        alert('Uspjesna trgovina');
+    }
+    
+    if(creditCardPayment.checked === true){
+        checkedCreditCard = true;
+    }
+    if(checkedCreditCard === true)
+    {
+        alert('Idi na novi page');
+    }
+}
+
+let backToPage = () => {
+    let paymentContainer = document.querySelector('.payment-container');
+    paymentContainer.remove();
+    realBody.style.overflowY = 'scroll';
+    userContainer.addEventListener('click',pageEdit);
+}
+
+let counter = 0;
+
+let showDetails = (element) =>{
+    let parentElement = element.parentElement;
+
+    let carImage = parentElement.querySelector('.carImage');
+    let carTitle = parentElement.querySelector('.carTitle');
+    let carPerInfo = parentElement.querySelector('.carPerInfo');
+    let carDetails = parentElement.querySelector('.carDetails');
+
+    let detailsDiv = parentElement.querySelector('.detailsInfo');
+    counter++;
+    if(counter % 2 !== 0){
+        detailsDiv.style.display = 'flex';
+        carImage.style.display = 'none';
+        carTitle.style.display = 'none';
+        carPerInfo.style.display = 'none';
+        carDetails.style.marginTop = "200px";
+    }
+    else{
+        detailsDiv.style.display = 'none';
+        carImage.style.display = 'block';
+        carTitle.style.display = 'block';
+        carPerInfo.style.display = 'flex';
+        carDetails.style.marginTop = "30px";
+    }
+    
+}
+randomUserName();
+
+//navigation to editProfile page
+let userContainer = document.querySelector('.user');
+
+let pageEdit = () =>{
+    alert('Idi na novi page');
+}
+
+userContainer.addEventListener('click',pageEdit);
+
+//responsive price range slider
+
+let priceSlider = document.querySelector('.slider input');
+let beginPrice = document.querySelector('.startPrice');
+priceSlider.addEventListener('change',()=>{
+    let priceValue = priceSlider.value;
+    beginPrice.innerText = priceValue + 'KM';
+})
+
+//filtering data
